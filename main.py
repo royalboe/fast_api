b1f138ea9354375c80234f91f45240866bf2cfad
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-05-02 10:57:09 +0100

    found a way to dynamically add id at the point of initialization

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.params import Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
 class Post(BaseModel):
+    id: int = Field(default_factory=lambda: randrange(0, 1_000_000)) # generates a value at the time of model creation
     title: str
     content: str
     published: bool = True
@@ -24,8 +26,8 @@ class Storage():
     
 storage = Storage()
 
-storage.add_post(Post(title="Post 1", content="Content 1"))
-storage.add_post(Post(title="Post 2", content="Content 2"))
+storage.add_post(Post(title="Post 1", content="Content 1").model_dump())
+storage.add_post(Post(title="Post 2", content="Content 2").model_dump())
 
 @app.get("/")
 async def root():
@@ -44,9 +46,9 @@ def create_posts(payload: Post = Body(...)):
     """
     # Validate the payload using Pydantic
     # payload.dict() To convert pydantic to dict
-    payload.model_dump() # To convert pydantic to dict in v2.0
-    # Simulate saving the post to a database or processing it 
-    print(payload)
+    post = payload.model_dump() # To convert pydantic to dict in v2.0
+    storage.add_post(post) # Simulate saving the post to a database or processing it 
+    print(post)
     return {
-        "data": payload
+        "data": post
         }
