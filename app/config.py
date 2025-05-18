f6a6b4813a5933ba35f8d067cdea7b1de2817a3b
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-05-18 16:25:07 +0100

    chnaged password

--- app/config.py
+++ app/config.py
@@ -13,5 +13,8 @@ class Settings(BaseSettings):
 
   model_config = SettingsConfigDict(env_file=".env")
 
+  # class Config:
+  #       env_file = ".env"
+
 
 settings = Settings()
---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from sqlmodel import SQLModel
+
+metadata = SQLModel.metadata
