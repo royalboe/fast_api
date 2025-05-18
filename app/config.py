fdb4433affed0dfffec5ecfbff7f48595856df67
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-05-18 03:48:59 +0100

    chaged config for env variables

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
   db_name: str
@@ -11,8 +11,7 @@ class Settings(BaseSettings):
   access_token_expire_minutes: int
   algorithm: str
 
-  class Config:
-    env_file = ".env"
+  model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
