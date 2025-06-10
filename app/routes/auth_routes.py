c0696f729ed43cdd762bd17793e23930b760d0d7
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-06-10 12:54:23 +0100

    added appropriate responses to login

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -13,17 +13,19 @@ router = APIRouter()
 def login(session: SessionDep, user_credentials: OAuth2PasswordRequestForm = Depends()):
 # def login(session: SessionDep, user_credentials: LoginSchema):
     
+    if not user_credentials.username or not user_credentials.password:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid credentials")
+
     user = session.exec(select(UserModel).where(UserModel.email == user_credentials.username)).first()
     
-    if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
+    if not user or not verify_password(user_credentials.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
 
-    if not verify_password(user_credentials.password, user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
-    
     # Create a JWT token here if needed
     access_token = create_access_token(data={"user_id": user.id})
-
     # Return token
-
     return {"access_token": access_token, "token_type": "bearer"}
